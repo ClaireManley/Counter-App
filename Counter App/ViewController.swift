4bad10854b8e927b42211931b42deedd7005a039
Claire Manley <cmanley2@zagmail.gonzaga.edu>
2020-02-08 12:22:54 -0800

    Counter App - Redo for homework 2/6.

--- Counter App/ViewController.swift
+++ Counter App/ViewController.swift
@@ -9,12 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var numberLabel: UILabel!
+    
+     var currentNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
+    @IBAction func lowerNumberPressed(_ sender: UIButton) {
+        print("This \(sender.tag) was pressed!")
 
+        currentNumber = currentNumber + sender.tag
+        numberLabel.text = String(currentNumber)
+    }
+    
+    @IBAction func higherNumberPressed(_ sender: UIButton) {
+        print("This \(sender.tag) was pressed!")
+
+              currentNumber = currentNumber + sender.tag
+              numberLabel.text = String(currentNumber)
+    }
 }
 
